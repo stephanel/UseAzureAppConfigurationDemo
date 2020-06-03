4870cf8d96839893c3e5ab04320a30303f0e515d
Stéphane Lasmartres <SLasmartres@idt.pf>
2020-06-02 16:13:30 -1000

    add source code

---
+++ UseAzureConfigurationTests/Controllers/ConfigController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UseAzureConfigurationTests.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ConfigController : ControllerBase
+    {
+        private readonly Settings _settings;
+
+        public ConfigController(Settings settings)
+        {
+            _settings = settings;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return Ok(_settings);
+        }
+    }
+}
